161f5f1a885ae87b25ced9dd4ff5a95ee3648100
Nitin SINGH <nitinkumar.singh@f5.com>
2021-04-09 13:13:08 +0530

    databinding - string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,12 +4,12 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]', // behave simalar to css atribute property
   // selector: '.app-servers', // this behaves simalar to css class property
   selector: 'app-servers', // recommended one
-  // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
   // template: '<app-server></app-server><app-server></app-server>',
-  template: `<app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>`,
-  styleUrls: ['./servers.component.css'],
+  // template: `<app-server></app-server>
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  // styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
   constructor() {}
