d25ae9d9c878c021a6290507482896b4add1f5da
Nitin SINGH <nitinkumar.singh@f5.com>
2021-04-11 17:19:27 +0530

    components and data binding- binding to custom property with input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { UsermgmtComponent } from './usermgmt/usermgmt.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, UsermgmtComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, UsermgmtComponent, CockpitComponent, ServerElementComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
