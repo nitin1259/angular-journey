0f69f40e53666a42112221ce0efdf3a49ec2bb8c
Nitin SINGH <nitinkumar.singh@f5.com>
2021-04-12 12:30:20 +0530

    components and data binding- odd even assignment game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onFiredInterval(input: number) {
+    // console.log('incoming fired input: ' + input);
+
+    if (input % 2 === 0) {
+      this.evenNumbers.push(input);
+    } else {
+      this.oddNumbers.push(input);
+    }
+  }
+}
